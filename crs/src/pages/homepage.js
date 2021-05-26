6ac0a00d6f2d76e0e7fbdd880512f5ee19b597d3
AmitShaw <yorsamit1@gmail.com>
2021-05-26 21:39:48 +0530

    added student signin authentication

--- crs/src/pages/homepage.js
+++ crs/src/pages/homepage.js
@@ -1,5 +1,6 @@
 import React, {useState, useEffect} from 'react';
 import { makeStyles } from '@material-ui/core/styles';
+import {BrowserRouter, Redirect, Switch, withRouter} from 'react-router-dom';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Container from '@material-ui/core/Container';
@@ -7,7 +8,6 @@ import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
 import Paper from '@material-ui/core/Paper'
 import Grid from '@material-ui/core/Grid'
-import {BrowserRouter as Router, Link, Route, Switch} from 'react-router-dom'
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
 
@@ -15,6 +15,7 @@ import SignIn from "../components/SignIn";
 import SignUp from "../components/SignUp";
 
 import JobFeed from "../Student/JobFeed";
+import axios from 'axios';
 
 
 
@@ -51,43 +52,64 @@ const useStyles = makeStyles((theme) => ({
   }
 }));
 
-export default function Homepage() {
+const Homepage = (props) => {
   const classes = useStyles();
   const [flag, setFlag] = useState(0);
-  const [signup, setSignup] = useState(false);
-  const [details, setDetails]=useState({});
+  const [cred, setCred]=useState({});
+  const [details, setDetails]=useState({signIn:false, Id:true})
+  const {history}=props;
+
+  const handleSubmit=(event)=>{
+    event.preventDefault();  
+    axios.post("http://localhost:5000/signIn", cred)
+    .then(result=>{
+      localStorage.setItem("token", result.data.token);
+      history.push(`/${result.data.user[0].id}`);
+      setDetails({signIn:true, Id:result.data.user[0].id});
+    })
+    .catch(err=>{
+      console.log(err);
+    })
+  }
 
   return (
-    <Router>
-      <div className={classes.root}>
-        <AppBar position="static" className={classes.appbar}>
-          <Toolbar>
-            <Typography variant="h6" className={classes.title}>
-              Campus Recruitment System
-            </Typography>
-            <Button color="inherit" >Login</Button>
-            <Button color="inherit">Statistics</Button>
-          </Toolbar>
-        </AppBar>
-        <Container >
-        <Grid container>
-          <Grid item xs={8}>
-            <div className={classes.front}>
-              <h2>IIESTs Campus Recruitment</h2>
-              <p>Join in to apply for placements</p>
-            </div>
+    <>
+      {details.signIn?
+        <BrowserRouter>
+          <Switch>
+            <Redirect from="/" to={`/${details.id}`}/>
+          </Switch>
+        </BrowserRouter>
+        :
+        <div className={classes.root}>
+          <AppBar position="static" className={classes.appbar}>
+            <Toolbar>
+              <Typography variant="h6" className={classes.title}>
+                Campus Recruitment System
+              </Typography>
+              <Button color="inherit" >Login</Button>
+              <Button color="inherit">Statistics</Button>
+            </Toolbar>
+          </AppBar>
+          <Container >
+          <Grid container>
+            <Grid item xs={8}>
+              <div className={classes.front}>
+                <h2>IIESTs Campus Recruitment</h2>
+                <p>Join in to apply for placements</p>
+              </div>
+            </Grid>
+            <Grid item xs={4}>
+              <Paper className={classes.signin}>
+                <SignIn flag={flag} setFlag={setFlag} cred={cred} setCred={setCred} handleSubmit={handleSubmit}/>
+              </Paper>
+            </Grid>
           </Grid>
-          <Grid item xs={4}>
-            <Paper className={classes.signin}>
-              <SignIn flag={flag} setFlag={setFlag}/>
-            </Paper>
-          </Grid>
-        </Grid>
-        </Container>
-      </div>
-      <Switch>
-        <Route exact path="/signup" render={()=><SignUp/>}></Route>
-      </Switch>
-    </Router>
+          </Container>
+        </div>
+      }
+    </>
   );
 }
+
+export default withRouter(Homepage);
