9a990a2ead45c2131c300bd9ae0cae7aa84d53d7
AmitShaw <yorsamit1@gmail.com>
2021-05-26 21:38:26 +0530

    added token authentication

--- crs/src/Student/Register/studentRegisterMain.js
+++ crs/src/Student/Register/studentRegisterMain.js
@@ -1,4 +1,6 @@
 import { Avatar, Grid, makeStyles, Paper } from '@material-ui/core';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 import StudentAppbar from '../studentAppBar';
 import StudentRegistration from './studentRegistration';
 
@@ -22,29 +24,60 @@ const useStyles = makeStyles((theme) => ({
     photoUpload: {
         marginTop: theme.spacing(5),
         marginBottom: theme.spacing(3),
+    },
+    greet:{
+        display:'flex', 
+        width:'100vw', 
+        height:'100vh', 
+        justifyContent:'center', 
+        alignItems:'center'
     }
   }));
 
 const StudentRegisterMain=()=>{
     const classes = useStyles();
+    const [access, setAccess]=useState(false);
+    const [msg, setMsg]=useState("Please wait, loading...")
+    useEffect(()=>{
+        const request=async()=>{
+            axios.get("http://localhost:5000/student_home", {
+                headers:{
+                    "x-access-token":localStorage.getItem("token"),
+                }
+            }).then(res=>{
+                setAccess(true);
+            }).catch(err=>{
+                setMsg("Session expired, please login again!");
+            })
+        }
+        request();
+    }, [])
     return(
-        <div className={classes.root}>
-            <StudentAppbar/>
-            <div className={classes.container}>
-                <Grid container spacing={3}>
-                    <Grid item xs={12} sm={4} className={classes.photoUpload}>
-                        <Paper>
-                            <div style={{display:'flex',justifyContent: 'center',alignItems: 'center',}}>
-                                <Avatar variant="rounded" alt="Remy Sharp" className={classes.avatar} src="https://qph.fs.quoracdn.net/main-qimg-99edf6c9b901fd291b7910ea0bbe8811" />
-                            </div>
-                        </Paper>
-                    </Grid>
-                    <Grid item xs={12} sm={8}>
-                        <StudentRegistration/>
-                    </Grid>
-                </Grid>
+        <>
+        {!access?
+            <div className={classes.greet}>
+                <h1>{msg}</h1>
             </div>
-        </div>
+            :
+            <div className={classes.root}>
+                <StudentAppbar/>
+                <div className={classes.container}>
+                    <Grid container spacing={3}>
+                        <Grid item xs={12} sm={4} className={classes.photoUpload}>
+                            <Paper>
+                                <div style={{display:'flex',justifyContent: 'center',alignItems: 'center',}}>
+                                    <Avatar variant="rounded" alt="Remy Sharp" className={classes.avatar} src="https://qph.fs.quoracdn.net/main-qimg-99edf6c9b901fd291b7910ea0bbe8811" />
+                                </div>
+                            </Paper>
+                        </Grid>
+                        <Grid item xs={12} sm={8}>
+                            <StudentRegistration/>
+                        </Grid>
+                    </Grid>
+                </div>
+            </div>
+            }
+        </>
     );
 }
 
