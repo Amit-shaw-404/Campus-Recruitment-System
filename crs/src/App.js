d666015c585cee706f5125850484e45a22e40875
AmitShaw <yorsamit1@gmail.com>
2021-05-26 21:37:29 +0530

    added Routing

--- crs/src/App.js
+++ crs/src/App.js
@@ -2,13 +2,18 @@ import logo from './logo.svg';
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
 import SignUp from './components/SignUp';
 import Homepage from './pages/homepage';
+import StudentRegisterMain from './Student/Register/studentRegisterMain';
+import AddJob from './Admin/addJob';
 
 function App() {
   return (
     <Router>
+      {/* <StudentRegisterMain/> */}
+      {/* <AddJob/> */}
       <Switch>
         <Route exact path="/" render={()=><Homepage/>}></Route>
         <Route exact path="/signup" render={()=><SignUp/>}></Route>
+        <Route exact path={`/:id`} component={StudentRegisterMain}></Route>
       </Switch>
     </Router>
   );
