9234a0581ba2d559ee0814059f4eb8633c9ca900
AmitShaw <yorsamit1@gmail.com>
2021-05-28 01:39:42 +0530

    added admin account schema

--- crs/backend/models/adminAccounts.js
+++ crs/backend/models/adminAccounts.js
@@ -3,4 +3,4 @@ const adminAccountTemplate=mongoose.Schema({
     email:{type:String, require:true},
     password:{type:String, require:true}
 })
-module.exports=mongoose.model("adminAccounts", adminAccountTemplate);
+module.exports=mongoose.model("adminaccounts", adminAccountTemplate);
