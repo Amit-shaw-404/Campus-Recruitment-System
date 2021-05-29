9f579c5b1243c3e053529b2cc5dc653b88ec8f4b
AmitShaw <yorsamit1@gmail.com>
2021-05-29 21:32:18 +0530

    Job feed completed

--- crs/src/Student/JobFeed.js
+++ crs/src/Student/JobFeed.js
@@ -2,13 +2,10 @@ import { makeStyles } from '@material-ui/core/styles';
 import Grid from '@material-ui/core/Grid';
 import Paper from '@material-ui/core/Paper'
 import FilterListIcon from '@material-ui/icons/FilterList';
-import StudentAppbar from './studentAppBar';
-import "../App.css"
 import { useEffect, useState } from 'react';
 import axios from 'axios';
 import {FormControlLabel, IconButton} from '@material-ui/core';
 import Switch from '@material-ui/core/Switch';
-import Checkbox from '@material-ui/core/Checkbox';
 import React from 'react';
 import Dialog from '@material-ui/core/Dialog';
 import DialogContent from '@material-ui/core/DialogContent';
@@ -16,9 +13,12 @@ import DialogTitle from '@material-ui/core/DialogTitle';
 import useMediaQuery from '@material-ui/core/useMediaQuery';
 import { useTheme } from '@material-ui/core/styles';
 import CloseIcon from '@material-ui/icons/Close';
+import {CircularProgress} from '@material-ui/core';
 
 
-import "../App.css"
+import category from "../List_Files/jobType.json"
+import place from "../List_Files/location.json"
+
 import JobFeedComponent from './JobFeedComponent';
 import JobDetails from './JobDetails';
 
@@ -77,46 +77,53 @@ export default function JobFeed({isAdmin}){
     category:'Business Analyst',
     workFromHome:false,
     location:'Gurgaon',
-    salary:'C',
+    companyRank:'C',
   });
+  const [isLoading, setIsLoading]=useState(true);
+  const [err, setErr]=useState(false);
 
   const [details, setDetails] = useState([{
-        jobTitle:"",
-        companyName:"",
-        location:"",
-        workFromHome:false,
-        startDate:"",
-        applyBy:"",
-        salary:"",
-        companyRank:"",
-        companyDescription:"",
-        jobDescription:"",
-        eligibility:"",
-        noOfOpening:"",
-        perks:"",
+   jobTitle:"",
+   companyName:"",
+   location:"",
+   workFromHome:false,
+   startDate:"",
+   applyBy:"",
+   salary:"",
+   companyRank:"",
+   companyDescription:"",
+   jobDescription:"",
+   eligibility:"",
+   noOfOpening:"",
+   perks:"",
   }]);
   const handleChange = (event) => {
     event.preventDefault();
     if(event.target.name==="workFromHome"){
-      console.log(event.target.name)
-      setFilter({...filter, [event.target.name]: event.target.checked })
+      if(event.target.checked)
+      setFilter({...filter, [event.target.name]: event.target.checked, ["location"]:'' })
+      else
+      setFilter({...filter, [event.target.name]: event.target.checked});
     }
     else{
       setFilter({...filter, [event.target.name]: event.target.value });
     }
-    console.log(filter);
   };
 
   useEffect(() => {
+    setIsLoading(true);
+    setErr(false);
     const id = setTimeout(()=>{
       const request=async()=>{
         const data=await axios.post('http://localhost:5000/jobFeed', filter)
         .then(result=>{
-          console.log(result);
+          setIsLoading(false);
+          setErr(false);
           setDetails(result.data);
         })
         .catch(err=>{
-          console.log(err);
+          setIsLoading(false);
+          setErr(true);
         })
       }
       request();
@@ -153,6 +160,7 @@ export default function JobFeed({isAdmin}){
             <div className={classes.input}>
               <p style={{margin:'0 0 10px 0'}}>Category</p>
               <select name="category" id="Category" value={filter.category} onChange={handleChange}>
+                  <option value="">All</option>
                   {category.map((option) => (
                     <option key={option.value} value={option.value}>
                       {option.label}
@@ -176,21 +184,17 @@ export default function JobFeed({isAdmin}){
               ? <div className={classes.input}>
                 <p style={{margin:'0 0 10px 0'}}>Location</p>
                 <select name="location" id="Location" value={filter.location} onChange={handleChange}>
-                    {place.map((option) => (
-                      <option key={option.value} value={option.value}>
-                        {option.label}
-                      </option>
-                    ))}
+                  <option value="" defaultValue>All</option>
+                  {place.map((option) => (
+                    <option key={option.value} value={option.value}>
+                      {option.label}
+                    </option>
+                  ))}
                 </select>
                 </div>
               : <div className={classes.input}>
                 <p style={{margin:'0 0 10px 0'}}>Location</p>
                 <select name="location" id="Location" value="" onChange={handleChange} disabled>
-                  {/* {place.map((option) => (
-                    <option key={option.value} value={option.value}>
-                      {option.label}
-                    </option>
-                  ))} */}
                 </select>
                 </div>
             }
@@ -198,29 +202,43 @@ export default function JobFeed({isAdmin}){
             <div className={classes.input}>
               <p style={{margin:'0 0 10px 0'}}>Salary</p>
               <select
-               name="salary"
+               name="companyRank"
                id="salary" 
                style={{margin:'0 0 50px 0'}} 
                value={filter.salary}
                onChange={handleChange}
               >
-                  {ranks.map((option) => (
-                    <option key={option.value} value={option.value}>
-                      {option.label}
-                    </option>
-                  ))}
+                <option value="" defaultValue>All</option>
+                {ranks.map((option) => (
+                  <option key={option.value} value={option.value}>
+                    {option.label}
+                  </option>
+                ))}
               </select>
             </div>
           </Paper>
         </Grid>
         <Grid item xs={12} md={7} className={classes.feed}>
-            {
+          {isLoading?
+            <div style={{width:'100%', height:'50%', display:'flex', justifyContent:'center', alignItems:'center' }}>
+              <div>
+                <CircularProgress style={{marginLeft:'25px'}}/>
+                <h2 style={{margin:'15px 0'}}>Searching...</h2>
+              </div>
+            </div>
+            :
+            (err?
+              <div style={{width:'100%', height:'50%', display:'flex', justifyContent:'center', alignItems:'center' }}>
+                <h2>No result found :(</h2>
+              </div>
+              :
               details.map((item, index)=>(
               <div key={index}>
                 <JobFeedComponent handleClick={handleClickOpen} item={item}/>
               </div>
               ))
-            }
+            )
+          }
         </Grid>
       </Grid>
         <Dialog
@@ -259,64 +277,4 @@ const ranks = [
       value: 'C',
       label: 'Type C [4-8 LPA]',
     },
-];
-const category = [
-    {
-      value: 'Full Stack Developer',
-      label: 'Full Stack Developer',
-    },
-    {
-      value: 'Machine Learning Engineer',
-      label: 'Machine Learning Engineer',
-    },
-    {
-      value: 'Front End Developer',
-      label: 'Front End Developer',
-    },
-    {
-      value: 'Backend Developer',
-      label: 'Backend Developer',
-    },
-    {
-      value: 'Business Analyst',
-      label: 'Business Analyst',
-    },
-    {
-      value: 'Android App Developer',
-      label: 'Android App Developer',
-    },
-    {
-      value: 'Data Scientist',
-      label: 'Data Scientist',
-    },
-];
-const place = [
-  {
-    value: 'Kolkata',
-    label: 'Kolkata',
-  },
-  {
-    value: 'Delhi',
-    label: 'Delhi',
-  },
-  {
-    value: 'Mumbai',
-    label: 'Mumbai',
-  },
-  {
-    value: 'Bangalore',
-    label: 'Bangalore',
-  },
-  {
-    value: 'Chennai',
-    label: 'Chennai',
-  },
-  {
-    value: 'Hyderabad',
-    label: 'Hyderabad',
-  },
-  {
-    value: 'Gurgaon',
-    label: 'Gurgaon',
-  },
 ];
